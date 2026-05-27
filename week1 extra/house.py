f64c12eac0273ee6440052cce8ba7d4e732a88b3
Harsh Vishwakarma <bettercallharshh@gmail.com>
2026-05-27 14:52:48 +0530

    Create house.py
    house.py — A Python program that prints a house-style pattern using loops and string formatting.

---
+++ week1 extra/house.py
@@ -0,0 +1,8 @@
+name = input("enter your name : ").lower()
+
+
+if name == "amit" or "harsh":
+    print(f"hello,  {name}")
+else:
+    print("who are you ?")
+    print("name" + name +  "does not found .")
