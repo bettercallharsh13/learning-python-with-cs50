89533f67bc2a56004fadd5b9f1e40d5664be293e
Harsh Vishwakarma <bettercallharshh@gmail.com>
2026-05-29 15:21:33 +0530

    Create hogwarts.py

---
+++ week2 extra/hogwarts.py
@@ -0,0 +1,9 @@
+x = [
+    {"name": "harsh", "place": "bhopal", "work": "nothing"},
+    {"name": "amit", "place": "indore", "work": None},
+]
+
+
+for y in x:
+    print(y["name"], y["place"], y["work"], sep="/")
+  
