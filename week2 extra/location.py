e34a87e4635d3acec2f1ef7aa275e4830e1e61d2
Harsh Vishwakarma <bettercallharshh@gmail.com>
2026-05-31 20:01:50 +0530

    Create location.py

---
+++ week2 extra/location.py
@@ -0,0 +1,8 @@
+def main():
+    latitude = float(input("enter your latitude : "))
+    longitude = float(input("enter your lnitude : "))
+    coordinate = latitude , longitude
+    print(f"latitude : {coordinate[0]}")
+    print(f"longitude : {coordinate[1]}")
+
+main()
