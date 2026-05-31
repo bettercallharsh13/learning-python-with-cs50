5e175bed61ba1a77c9ca7ee9dcc2e7febece4171
Harsh Vishwakarma <bettercallharshh@gmail.com>
2026-05-31 20:04:03 +0530

    Create mario.py

---
+++ week3 extra/mario.py
@@ -0,0 +1,13 @@
+def main():
+    height = int(input("enter height: "))
+    pyramid(height)
+
+def pyramid(n):
+    for i in range(n):
+
+        print("#" * (i +1))
+
+
+if __name__ ==  "__main__":
+    main()
+
