62f3eb65fc62731de3c89dcc9b0851827271b96a
Harsh Vishwakarma <bettercallharshh@gmail.com>
2026-06-12 18:14:35 +0530

    Add number guessing game in game.py
    Implement a number guessing game with user input for difficulty level.

---
+++ week4/game.py
@@ -0,0 +1,29 @@
+
+from random import randint
+
+while True:
+    try:
+        level = int(input("Level: "))
+        if level > 0:
+            break
+    except ValueError:
+        pass
+
+number = randint(1, level)
+
+while True:
+    try:
+        guess = int(input("Guess: "))
+        if guess <= 0:
+            continue
+
+        if guess < number:
+            print("Too small!")
+        elif guess > number:
+            print("Too large!")
+        else:
+            print("Just right!")
+            break
+
+    except ValueError:
+        pass
